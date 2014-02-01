fbcbe1d498161c6d27051ee41c339cf0b6edb090
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-01 21:21:21 +0200

    Update setup script and classifiers.

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 # -*- encoding: utf8 -*-
-from setuptools import setup, find_packages
-
 import os
 
+from setuptools import find_packages, setup
+
 setup(
-    name = "django-prefetch",
-    version = "0.1.1",
-    url = 'https://github.com/ionelmc/django-prefetch',
-    download_url = '',
-    license = 'BSD',
-    description = "Generic model related data prefetch framework for Django",
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    author = 'Ionel Cristian Mărieș',
-    author_email = 'contact@ionelmc.ro',
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    py_modules = ['prefetch'],
-    include_package_data = True,
-    zip_safe = False,
-    classifiers = [
+    name="django-prefetch",
+    version="0.2",
+    url='https://github.com/ionelmc/django-prefetch',
+    download_url='',
+    license='BSD',
+    description="Generic model related data prefetch framework for Django",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Ionel Cristian Mărieș',
+    author_email='contact@ionelmc.ro',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=['prefetch'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
@@ -26,5 +26,14 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
-    ]
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    install_requires=['Django>=1.1']
 )
