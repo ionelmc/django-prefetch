04f14ab6ce56a7a014ef9a40f3bc0b1f7ccab85c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-16 14:49:04 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
